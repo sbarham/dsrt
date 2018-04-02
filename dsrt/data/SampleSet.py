655ade51201271689bf75413d3466bda6b1395ac
Samuel Barham <sbarham@terpmail.umd.edu>
2018-04-02 03:27:29 -0400

    Got corpus commands working perfectly; began work on prepare command

--- dsrt/data/SampleSet.py
+++ dsrt/data/SampleSet.py
@@ -9,44 +9,51 @@ import logging
 from dsrt.config.defaults import DataConfig
 
 class SampleSet:
-    def __init__(self, dialogues=None, vectorizer=None, properties=None, config=DataConfig(),
+    def __init__(self, dialogues=None, enc_dec_splitter=None, properties=None, config=DataConfig(),
                  preprocessed=False, name=None, f=None):
+        # load the config
+        self.config = config
+
         # initialize the logger
         self.init_logger()
-        
-        self.config = config
-        self.vectorizer = vectorizer
+
+        self.enc_dec_splitter = enc_dec_splitter
         self.properties = properties
-        
+
         # read in dialogues
         self.dialogues = np.swapaxes(dialogues, 0, 1)
         self.length = len(dialogues)
-        
+
         if preprocessed:
         	self.load_sampleset(f, name)
         	return
-        
+
         # make encoder/decoder split
-        self.encoder_x = self.dialogues[0]
-        self.decoder_x = self.dialogues[1]
-        self.decoder_y = self.dialogues[2]
-        
+        split = self.enc_dec_splitter.transform(self.dialogues)
+        self.encoder_x = split[0]
+        self.decoder_x = split[1]
+        self.decoder_y = split[2]
+
+        print("Got encoder_x = {}".format(self.encoder_x))
+        print("Got decoder_x = {}".format(self.decoder_x))
+        print("Got decoder_y = {}".format(self.decoder_y))
+
     def init_logger(self):
         self.logger = logging.getLogger()
         self.logger.setLevel(self.config['logging-level'])
-            
+
     def save_sampleset(self, f, name):
         '''Serialize the sampleset to file using the HDF5 format. Name is usually in {train, test}.'''
         f.create_dataset(name + '_encoder_x', data=self.encoder_x)
         f.create_dataset(name + '_decoder_x', data=self.decoder_x)
         f.create_dataset(name + '_decoder_y', data=self.decoder_y)
-        
+
     def load_sampleset(self, f, name):
         '''Read the sampleset from using the HDF5 format. Name is usually in {train, test}.'''
         self.encoder_x = np.array(f[name + '_encoder_x'])
         self.decoder_x = np.array(f[name + '_decoder_x'])
         self.decoder_y = np.array(f[name + '_decoder_y'])
-    
+
     def log(self, priority, msg):
         """
         Just a wrapper, for convenience.
