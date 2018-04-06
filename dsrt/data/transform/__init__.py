58216a9f84776f97bdd5733dd4657449abf58fea
Samuel Barham <sbarham@terpmail.umd.edu>
2018-04-06 06:36:42 +0000

    Purged spurious dataset from history and retested relevant packages

--- dsrt/data/transform/__init__.py
+++ dsrt/data/transform/__init__.py
@@ -4,7 +4,4 @@ from . Filter import Filter
 from . Padder import Padder
 from . Tokenizer import Tokenizer
 from . Vectorizer import Vectorizer
-<<<<<<< HEAD
 from . Masker import Masker
-=======
->>>>>>> 862dff8777db5d01aa74b25c93cb60f1514e208d
